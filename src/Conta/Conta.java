2e6658e3ea969db09964a9c704acdb01e0d219e6
Fabrinio <fabriniolucas1412@gmail.com>
2022-09-12 16:06:40 -0300

    Implementação de Conta Corrente, depositar e sacar

--- src/Conta/Conta.java
+++ src/Conta/Conta.java
@@ -1,16 +1,22 @@
 package Conta;
 
 public abstract class Conta {
-	private String nome;
-	private String cpf;
-	private String senha;
+	protected String nome;
+	protected String cpf;
+	protected String senha;
+	protected double saldo;
 	private double saque;
 	private double deposito;
 	private double transferencia;
 	
 	public Conta() {	
 	}
-	public Conta(String nome, String cpf, String senha, double saque, double deposito, double transferencia) {	
+	public Conta(String nome, String cpf, String senha, double saldo) {	
+		 super();
+		 this.nome = nome;
+		 this.cpf = cpf;
+		 this.senha = senha;
+		 this.saldo = saldo;
 	}
 	
 	public String getNome() {
@@ -37,15 +43,16 @@ public abstract class Conta {
 	public double getSaque() {
 		return saque;
 	}
-	public void setSaque(double saque) {
-		this.saque = saque;
+	public void Saque(double saque) {
+		this.saldo = saldo - saque;
 	}
 	
 	public double getDeposito() {
 		return deposito;
 	}
-	public void setDeposito(double deposito) {
-		this.deposito = deposito;
+	
+	public void Deposito(double deposito) {
+		this.saldo = saldo + deposito;
 	}
 	
 	public double getTransferencia() {
@@ -54,5 +61,16 @@ public abstract class Conta {
 	public void setTransferencia(double transferencia) {
 		this.transferencia = transferencia;
 	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	@Override
+	public String toString() {
+		return "[Nome = " + nome + ", CPF = " + cpf + ", Saldo = " + saldo + "]";
+	}
+	
 	
 }
--- src/Main/SistemaInterno.java
+++ src/Main/SistemaInterno.java
@@ -1,10 +1,16 @@
 package Main;
 
+import Conta.ContaCorrente;
+
 public class SistemaInterno {
 
 	public static void main(String[] args) {
-		
-
+		ContaCorrente c1 = new ContaCorrente("Lucas", "05397119342", "1234",1400.50, "Conta Corrente");
+		System.out.println(c1.toString());
+		c1.Deposito(10.00);
+		System.out.println(c1.getSaldo());
+		c1.Saque(410.50);
+		System.out.println(c1.getSaldo());
 	}
 
 }
