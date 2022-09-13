102e9bb76150cf3bc29a43d0a3e53fc616992905
brunakbral <bruninha_kbral@hotmail.com>
2022-09-12 21:32:43 -0300

    Implementação de Funcionário

--- src/Funcionarios/Funcionario.java
+++ src/Funcionarios/Funcionario.java
@@ -1,5 +1,36 @@
 package Funcionarios;
 
 public abstract class Funcionario {
+	private String nome;
+	private String cpf;
+	private String senha;
+	private String cargo;
+	
+	
+	public Funcionario(String nome, String cpf, String senha, String cargo) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.senha = senha;
+		this.cargo = cargo;
+		}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	
 }
+;
